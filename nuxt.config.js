4afa081b637a65cf2fa2cccc6d2781cd433453be
DeVoresyah ArEst <de.voresyah@gmail.com>
2022-10-02 11:13:14 +0800

    fix: default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hired-todo-vue',
+    title: 'Todo App List',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
